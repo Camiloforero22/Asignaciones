f5cc586695b2fe4731fedd728234053da7138911
Juan Camilo Peña Forero <juancamilopenaforero@MacBook-Air-de-Juan.local>
2021-11-17 20:47:20 -0500

    Asignaciones de equipos, segundo commir

--- src/datasources/mongo.datasource.ts
+++ src/datasources/mongo.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'Mongo',
   connector: 'mongodb',
-  url: 'mongodb+srv://Developer:C4m1l021@cluster0.j2v1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  url: 'mongodb+srv://Developer:C4m1l021@cluster0.j2v1t.mongodb.net/Asignaciones?retryWrites=true&w=majority',
   host: '',
   port: 0,
   user: '',
